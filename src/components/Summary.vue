c666d3eb61fc59d636fc8cc3739c495cae19c892
DryKillLogic <drykilllogic@gmail.com>
2025-06-11 18:09:54 -0700

    - Added AIOLists as a catalogs provider with full multilanguage support. - Removed TMDB and Trakt addons. - Added Italian and German presets.

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -42,12 +42,8 @@
           <p>Stremify</p>
         </div>
         <div class="col-2 text-center">
-          <img src="/public/logos/trakt.png" alt="Trakt TV" />
-          <p>Trakt TV</p>
-        </div>
-        <div class="col-2 text-center">
-          <img src="/public/logos/tmdb.png" alt="TMDB" />
-          <p>TMDB</p>
+          <img src="/public/logos/aiolists.png" alt="AIOLists" />
+          <p>AIOLists</p>
         </div>
         <div class="col-2 text-center">
           <img src="/public/logos/cinemeta.png" alt="Cinemeta" />
@@ -64,12 +60,12 @@
           />
           <p>OpenSubtitles Pro</p>
         </div>
-      </div>
-      <div class="row">
         <div class="col-2 text-center">
           <img src="/public/logos/opensubtitles.png" alt="OpenSubtitles v3" />
           <p>OpenSubtitles v3</p>
         </div>
+      </div>
+      <div class="row">
         <div class="col-2 text-center">
           <img src="/public/logos/localfiles.png" alt="Local Files" />
           <p>Local Files</p>
@@ -124,7 +120,7 @@
       </li>
       <li>
         Customize the addons preset (optional). You have the option to remove or
-        sort addons, and rename or delete the Trakt/Anime Kitsu catalogs.
+        sort addons, and rename or delete the AIOLists/Anime Kitsu catalogs.
       </li>
       <li>
         Once you have finished customizing, click "Sync to Stremio" to update
